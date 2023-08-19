0e39fcade25cabfab5067c705f2ddde6dbbf75ff
Linda Wang <61102300+ylw311@users.noreply.github.com>
2023-08-19 01:10:26 -0400

    changed eyeball css to component

--- client/src/components/EyeExercises/EyeExercises.jsx
+++ client/src/components/EyeExercises/EyeExercises.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 function EyeExercises() {
+  const blackScreenStyles = {
+    width: '100vw',
+    height: '100vh',
+    top: 0,
+    backgroundColor: 'black',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+    fontSize: '24px',
+  };
+
   return (
-    <div>EyeExercises</div>
-  )
+    <div style={blackScreenStyles}>
+      EyeExercises
+    </div>
+  );
 }
 
-export default EyeExercises
+export default EyeExercises;
