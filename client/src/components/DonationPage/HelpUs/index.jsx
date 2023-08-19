1c1cdabe29b3e31b254ea3d44ef36cb0cc8e92d2
ricsign <ricsign123@gmail.com>
2023-08-19 18:13:23 -0400

    Donation to home page

--- client/src/components/DonationPage/HelpUs/index.jsx
+++ client/src/components/DonationPage/HelpUs/index.jsx
@@ -1,14 +1,22 @@
-import classes from './HelpUs.module.css';
-import HelpUsTitle from './HelpUsTitle';
+import classes from "./HelpUs.module.css";
+import HelpUsTitle from "./HelpUsTitle";
+
+import { HomeOutlined } from "@ant-design/icons";
 
 const HelpUs = () => {
   return (
     <section className={classes.HelpUs}>
       <HelpUsTitle />
       <p className={classes.HelpUsParagraph}>
-      Every contribution brings the gift of sight closer to those in need. Your support funds life-changing initiatives, restoring vision in third-world nations. Join us in illuminating lives with hope and clarity.  </p>
-      
-    
+        Every contribution brings the gift of sight closer to those in need.
+        Your support funds life-changing initiatives, restoring vision in
+        third-world nations. Join us in illuminating lives with hope and
+        clarity.{" "}
+      </p>
+      <HomeOutlined
+        className="mt-10 text-white text-2xl text-gray-800 font-extrabold hover:scale-150 transition-transform duration-300 translate-y-[-3px]"
+        onClick={() => (window.location.href = "/home")}
+      />
     </section>
   );
 };
